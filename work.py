c8e85f6a477f649394a3d183bb874194d8cbd13c
x12lh3art <59593576+x12lh3art@users.noreply.github.com>
2020-01-07 10:59:02 +0545

    Update work.py

--- work.py
+++ work.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import os
 import time
 
-useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/74.0.3729.157 Safari/537.36"
+useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
